c5c5ed291eb63962b6aed31e3c3a986a6c1d33ce
Emmanuel Obi <emmanuel@ivan.local>
2016-09-01 16:34:13 -0500

    fix java issue which fixes ES which fixes Kibana issue

--- provision.sh
+++ provision.sh
@@ -14,8 +14,9 @@
 # --- JAVA8
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
-#sudo apt-get install oracle-java8-set-default
+sudo echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get -y install oracle-java8-installer
+sudo apt-get -y install oracle-java8-set-default
 # --- ELASTICSEARCH
 sudo wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
