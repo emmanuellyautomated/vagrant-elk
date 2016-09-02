b552acf1a899925e515eaa26d11c9c3a47c04d1e
Emmanuel Obi <emmanuel@ivan.local>
2016-09-01 20:44:10 -0500

    nix unnecessary terminal vommit from java

--- provision.sh
+++ provision.sh
@@ -15,8 +15,9 @@
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get -y install oracle-java8-installer
-sudo apt-get -y install oracle-java8-set-default
+sudo echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get -y -qq install oracle-java8-installer > /dev/null 2>&1
+sudo apt-get -y -qq install oracle-java8-set-default > /dev/null 2>&1
 # --- ELASTICSEARCH
 sudo wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
